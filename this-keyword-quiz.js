2a056c9ed8cf4e3e15ddf45830d94d72035b9ba5
CJ Powers <cjpwrs@gmail.com>
2016-05-17 12:47:43 -0600

    first draft done

--- this-keyword-quiz.js
+++ this-keyword-quiz.js
@@ -1,11 +1,12 @@
 //1) What is the value of 'this' at Line A? Why?
+//The object window, because we are in the context of the global scope
 
 if (true) {
     // Line A
 }
 
 //2) What is the value of this at line B when obj.staticFunction() is executed? Why?
-
+// 'this' is the obj object because that is the object on the left of the dot when staticFunction is invoked
 	var obj = {
 	    someData: "a string"
 	};
@@ -19,6 +20,7 @@ if (true) {
 	obj.staticFunction();
 
 //3) What is the value of this at line C? Why?
+// 'this' is the obj object because is resides within the scope of obj
 
 	var obj = {
 	    myMethod : function () {
@@ -29,6 +31,7 @@ if (true) {
 	myFun();
 
 //4) What is the value of this at line D? Why?
+// 'this' is the obj object because when myFun is invoked, it is in the scope of the obj object
 
 	function myFun() {
     // Line D
@@ -41,6 +44,7 @@ if (true) {
 	obj.myMethod();
 
 //5) What is the value of this at line E?
+//'this' is the obj object bacause the call() method sets 'this' equal to obj
 
 	function myFun() {
 	    // Line E
